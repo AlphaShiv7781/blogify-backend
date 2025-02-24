3993c68bbfd6418bc4397f96df7a2db379b67f50
Shivam <shivamsharma242424@gmail.com>
2025-02-25 02:08:47 +0530

    pushed

--- authJWT/users/serializers.py
+++ authJWT/users/serializers.py
@@ -8,10 +8,12 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password':{'write_only':True}}
 
     def create(self, validated_data):
-        password = validated_data.pop('password',None)  
+        password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
-            instance.set_password(password)
-
+            instance.set_password(password)  # Hash the password correctly
         instance.save()
         return instance
+
+
+       
